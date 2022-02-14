1683a7a014eb5c8bc35bcda821a09b8a91f6638a
yeatsg <yeatsg@gmail.com>
2022-02-14 11:04:22 -0500

    first push

---
+++ app.js
@@ -0,0 +1,62 @@
+// ℹ️ Gets access to environment variables/settings
+// https://www.npmjs.com/package/dotenv
+require("dotenv/config");
+
+// ℹ️ Connects to the database
+require("./db");
+
+// Handles http requests (express is node js framework)
+// https://www.npmjs.com/package/express
+const express = require("express");
+
+// Handles the handlebars
+// https://www.npmjs.com/package/hbs
+const hbs = require("hbs");
+
+const app = express();
+
+const bcrypt = require("bcryptjs");
+
+// ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
+require("./config")(app);
+
+// default value for title local
+const projectName = "blah";
+const capitalized = (string) =>
+  string[0].toUpperCase() + string.slice(1).toLowerCase();
+
+app.locals.title = `${capitalized(projectName)} created with IronLauncher`;
+
+// 👇 Start handling routes here
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      sameSite: "lax",
+      maxAge: 600000,
+      // secure: true
+    },
+    store: MongoStore.create({
+      mongoUrl: "mongodb://localhost/signup-test",
+      ttl: 600000,
+    }),
+  })
+);
+
+const index = require("./routes/index");
+app.use("/", index);
+const admin = require("./routes/admin");
+app.use("/admin", admin);
+const createPost = require("./routes/create-post");
+app.use("/create-post", createPost);
+
+// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
+require("./error-handling")(app);
+
+module.exports = app;
